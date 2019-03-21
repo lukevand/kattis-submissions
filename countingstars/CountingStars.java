c30fdc5a162794a75e77e1df2b5b7929c3511311
luke <lvkd@tfwno.gf>
2019-03-21 19:18:59 +1300

    countingstars done

--- countingstars/CountingStars.java
+++ countingstars/CountingStars.java
@@ -1,11 +1,9 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.io.IOException;
 
-public class CountingStars {
-
-    public void flood(char picture[][], int m, int n, int i, int j) {
+class CountingStars {
+    private static void flood(char picture[][], int m, int n, int i, int j) {
         if (i < 0 || i >= m || j < 0 || j >= n) {
             return;
         } else if (picture[i][j] == '#') {
@@ -23,38 +21,35 @@ public class CountingStars {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String line;
-        line = reader.readLine();
-        String[] tmp = line.split(" ");
-        int m = Integer.parseInt(tmp[0]);
-        int n = Integer.parseInt(tmp[1]);
-        // System.out.println(line);
-        // char c;
-        // int m, n;
-        // int count = 0;
-        // cin >> m;
-        // cin >> n;
-        // char picture[m][n];
-        // for (int i = 0; i < m; i++) {
-        //     for (int j = 0; j < n; j++) {
-        //         cin >> c; picture[i][j] = c;
-        //     }
-        // }
-        // /* for (int i = 0; i < m; i++) { */
-        // /*     for (int j = 0; j < n; j++) { */
-        // /*         cout << picture[i][j]; */
-        // /*     } */
-        // /*     cout << endl; */
-        // /* } */
+        int case_num = 0;
+        while ((line = reader.readLine()) != null) {
+            int count = 0;
+            case_num++;
+            String[] tmp = line.split(" ");
+            int m = Integer.parseInt(tmp[0]);
+            int n = Integer.parseInt(tmp[1]);
+            char[][] picture = new char[m][n];
 
-        // for (int i = 0; i < m; i++) {
-        //     for (int j = 0; j < n; j++) {
-        //         if (picture[i][j] == '-') {
-        //             count++;
-        //             /* cout << "flood(picture, m, n, i, j);" << endl; */
-        //             flood(picture, m, n, i, j);
-        //         }
-        //     }
-        // }
+            for (int i = 0; i < m; i++) {
+                line = reader.readLine();
+                // uncomment to print picture
+                // System.out.println(line);
+                for (int j = 0; j < n; j++) {
+                    picture[i][j] = line.charAt(j);
+                }
+            }
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (picture[i][j] == '-') {
+                        count++;
+                        flood(picture, m, n, i, j);
+                    }
+                }
+            }
+
+            System.out.printf("Case %d: %d\n", case_num, count);
+        }
 
     }
 }
