817aa56c6bbc226d936269111aca32860533cac6
luke <lukevanduuren@gmail.com>
2019-10-01 19:03:42 +1300

    Update

--- nikola.cpp
+++ nikola.cpp
@@ -10,12 +10,20 @@ typedef vector<VI> VVI;
 typedef vector<VII> VVII;
 #define INF 1e9
 
+struct hash_pair {
+    size_t operator()(const II& p) const {
+        return hash<int>{}(p.first) ^ hash<int>{}(p.second);
+    }
+};
+
 int main()
 {
     int N, a;
+    int m = INF;
     scanf("%d", &N);
     VVII g(N, VII());
-    VVI dist(N, VI(N, INF));
+    unordered_map<II, int, hash_pair> dist;
+
     VI cost(N, INF);
     for (int i=0; i<N; i++){
         scanf("%d", &a);
@@ -23,27 +31,28 @@ int main()
     }
 
     priority_queue<III, vector<III>, greater<III>> pq;
-    dist[0][0] = 0;
+    dist[II(0,0)] = 0;
     pq.push(III(0, II(0, 0)));
     while (!pq.empty()) {
         III front = pq.top(); pq.pop();
         int d = front.first;
         int u = front.second.first, lastJump = front.second.second;
-        cout << u << " " << lastJump << '\n';
-        if (d > dist[u][lastJump]) continue;
+        if (d > dist[II(u,lastJump)]) continue;
         VI next = {abs(lastJump)+1, -abs(lastJump)};
         for (int nextJump : next) {
-            cout << "next: " << nextJump << '\n';
             if (u+nextJump >= N || u+nextJump < 0) continue;
-            if (dist[u][lastJump] + cost[u+nextJump] < dist[u+nextJump][nextJump]) {
-                dist[u+nextJump][nextJump] = dist[u][lastJump] + cost[u+nextJump];
-                pq.push(III(dist[u+nextJump][nextJump], II(u+nextJump, nextJump)));
+            int &v = dist[II(u+nextJump,nextJump)];
+            if (v == 0) { v = INF; };
+            if (dist[II(u,lastJump)] + cost[u+nextJump] < dist[II(u+nextJump,nextJump)]) {
+                dist[II(u+nextJump,nextJump)] = dist[II(u,lastJump)] + cost[u+nextJump];
+                pq.push(III(dist[II(u+nextJump,nextJump)], II(u+nextJump, nextJump)));
+                if (nextJump > 0 && u+nextJump == N-1) {
+                    m = min(m, dist[II(u+nextJump,nextJump)]);
+                    printf("%d\n", m);
+                    return 0;
+                }
             }
         }
-    }
-    int m = INF;
-    for (int x : dist[N-1]) {
-        m = min(m, x);
     }
     printf("%d\n", m);
 }
